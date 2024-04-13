f24be5fe3e5ad596f36b278885b58efc4f449a5c
Celyne <dianakydd2@gmail.com>
2024-04-13 09:42:07 +0300

    Updated my view file

--- django_app_plp/src/main/views.py
+++ django_app_plp/src/main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def landing_view(request):
+    return HttpResponse("<h1>Welcome to Hive!</h1>")
