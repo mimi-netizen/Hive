06d4b0764efe2684912749ffbd11394ff872f3df
Celyne <dianakydd2@gmail.com>
2024-04-24 11:01:09 +0300

    Added CSS and Javascript

--- django_app_plp/src/main/views.py
+++ django_app_plp/src/main/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def landing_view(request):
-    return HttpResponse("<h1>Welcome to Hive!</h1>")
+def main_view(request):
+    return render(request, "views/main.html", {"name" : "Hive"})
