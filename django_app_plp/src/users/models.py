c37c9b36b6a45ebdcba185e7b67f7fc15525c408
Celyne <dianakydd2@gmail.com>
2024-04-29 14:44:35 +0300

    Installed localflavor

--- django_app_plp/src/users/models.py
+++ django_app_plp/src/users/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Location(models.Model):
+    address_1 = models.CharField(max_length=128)
+    address_2 = models.CharField(max_length=128, blank=True)
+    city = models.CharField(max_length=64)
+   
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo = models.ImageField(null=True)
