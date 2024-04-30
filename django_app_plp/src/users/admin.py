663476cda168345c09a0103167d4714f645d1402
Celyne <dianakydd2@gmail.com>
2024-04-30 10:28:50 +0300

    Added Location

--- django_app_plp/src/users/admin.py
+++ django_app_plp/src/users/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Location
 
 class ProfileAdmin(admin.ModelAdmin):
     pass
 
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Location, LocationAdmin)
