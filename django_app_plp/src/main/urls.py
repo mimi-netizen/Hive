27a3814c7a157b7d9f450515d69c5c2aed12ae35
Celyne <dianakydd2@gmail.com>
2024-06-07 13:18:59 +0300

    Created List View

--- django_app_plp/src/main/urls.py
+++ django_app_plp/src/main/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.urls import path
 
-from .views import main_view, home_view
+from .views import main_view, home_view, list_view
 
 urlpatterns = [
      path('', main_view, name = 'main'),
-     path('home/', home_view, name='home')
+     path('home/', home_view, name='home'),
+     path('list/', list_view, name='list')
+     
 ]
--- django_app_plp/src/main/views.py
+++ django_app_plp/src/main/views.py
@@ -15,3 +15,29 @@ def home_view(request):
         'listings': listings,
     }
     return render(request, "views/home.html", context)
+
+@login_required
+def list_view(request):
+    if request.method == 'POST':
+        try:
+            listing_form = ListingForm(request.POST, request.FILES)
+            location_form = LocationForm(request.POST, )
+            if listing_form.is_valid() and location_form.is_valid():
+                listing = listing_form.save(commit=False)
+                listing_location = location_form.save()
+                listing.seller = request.user.profile
+                listing.location = listing_location
+                listing.save()
+                messages.info(
+                    request, f'{listing.model} Listing Posted Successfully!')
+                return redirect('home')
+            else:
+                raise Exception()
+        except Exception as e:
+            print(e)
+            messages.error(
+                request, 'An error occured while posting the listing.')
+    elif request.method == 'GET':
+        listing_form = ListingForm()
+        location_form = LocationForm()
+    return render(request, 'views/list.html', {'listing_form': listing_form, 'location_form': location_form, })
