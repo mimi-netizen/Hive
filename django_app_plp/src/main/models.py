262ff9305b25dce7c938d0dd2f2923ec371c84c0
Celyne <dianakydd2@gmail.com>
2024-06-07 11:41:07 +0300

    Added Listing Data

--- django_app_plp/src/main/models.py
+++ django_app_plp/src/main/models.py
@@ -25,6 +25,9 @@ class Listing(models.Model):
     location = models.OneToOneField(
         Location, on_delete=models.SET_NULL, null=True)
     image = models.ImageField(upload_to=user_listing_path)
+    
+    def __str__(self):
+        return f'{self.seller.user.username}\'s Listing - {self.model}'
 
     
     
