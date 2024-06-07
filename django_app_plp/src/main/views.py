f57d96b7d3559890042a84971f4e95adf98d0ddb
Celyne <dianakydd2@gmail.com>
2024-06-07 12:33:43 +0300

    Adding Section Header HomePage

--- django_app_plp/src/main/views.py
+++ django_app_plp/src/main/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from .models import Listing
+
 # Create your views here.
 def main_view(request):
     return render(request, "views/main.html", {"name" : "Hive"})
 
 @login_required
 def home_view(request):
-    return render(request, "views/home.html")
+    listings = Listing.objects.all()
+    context = {
+        'listings': listings,
+    }
+    return render(request, "views/home.html", context)
