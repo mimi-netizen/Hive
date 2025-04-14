a8b5e1c917eaec602673723b0035097289b66edd
Celyne <dianakydd2@gmail.com>
2025-04-14 10:01:04 +0300

    requirements

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ cd django_app_plp
 pip install --upgrade pip
 pip install -r requirements.txt --no-cache-dir
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 python manage.py migrate
