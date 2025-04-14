758c6457217950b3d07df49f9bb70c4668e8c512
Celyne <dianakydd2@gmail.com>
2025-04-14 08:50:14 +0300

    Fix crispy-forms installation and configuration

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 cd django_app_plp
-pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt --no-cache-dir
 
 python manage.py collectstatic --no-input
 python manage.py migrate
